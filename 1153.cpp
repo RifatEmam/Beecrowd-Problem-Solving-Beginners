903696db3e65d572231406b37df66b78786669a3
M.D Rifat Emam <153221770+RifatEmam@users.noreply.github.com>
2025-05-30 17:06:21 +0600

    1153

--- 1153.cpp
+++ 1153.cpp
@@ -1,31 +1,36 @@
 #include<iostream>
 using namespace std;
-int fact(int n){
+int fact(int n)
+{
 
-if(n==0 || n==1){
-    return 1;
+    if(n==0 || n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+
+
+
 }
-else {
-    return n*fact(n-1);
-}
+int main ()
+{
 
-
-
-}
-int main (){
-
- int n;
+    int n;
     cin>>n;
     int i;
-if(0<n && n<13){
+    if(0<n && n<13)
+    {
 
-     i=fact(n);
+        i=fact(n);
 
-cout<<i;
+        cout<<i<<endl;
 
-}
+    }
 
 
-return 0;
+    return 0;
 }
 
